1e56c6eb0b61a8fa5d403b763820ce192490175f
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-10-06 09:59:29 +0900

    add chef_handler to dependencies

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,4 @@ license          'MIT License'
 description      'Installs/Configures slack-handler'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends          'chef_handler'
