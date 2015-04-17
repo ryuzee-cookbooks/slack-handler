6fbc15c657faa1efb3d4f64861060d3f581bf38a
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-04-18 07:05:32 +0900

    add post_on_success option

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 default['slack-handler']['channel'] = '#chef'
 default['slack-handler']['slack_webhook_url'] = ''
 default['slack-handler']['username'] = 'chef'
-default['slack-handler']['icon_emoj'] = ':chef:'
+default['slack-handler']['icon_emoji'] = ':chef:'
+default['slack-handler']['post_on_success'] = false
